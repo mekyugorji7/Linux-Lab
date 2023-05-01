8fbf27056d5f38b326cf5f63aea9bf65299f5ee1
Meky Ugorji <mekyugorji@Mekys-MacBook-Air.local>
2023-05-01 07:01:22 -0400

    Fixed error where file was empty

--- mycipher.py
+++ mycipher.py
@@ -1,1 +1,39 @@
+#!/usr/bin/python3
 
+import sys
+
+if len(sys.argv) == 2:
+	key = sys.argv[1]
+	message = input()
+	message = message.upper()
+	alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	onlyAlpha = ""
+	
+	for i in range(len(message)):
+		if message[i].isalpha():
+			onlyAlpha += message[i]
+	cipherWord = ""
+	for i in range(len(onlyAlpha)):
+		index_val = alphabets.index(onlyAlpha[i]) + int(key)
+		cipherWord += alphabets[index_val]
+	
+	count = 0
+	newLinecount = 0
+	for i in range(len(message)):
+		count += 1
+		if count == 6:
+			cipherWord = cipherWord[:i] + " " + cipherWord[i:]
+			count = 0
+			newLinecount += 1
+			if newLinecount == 10:
+				cipherWord = cipherWord[:i]+"\n"+cipherWord[i:]
+				newLinecount = 0
+				count -= 1
+	print(cipherWord)
+
+
+else:
+	print("Usage: python3 mycipher.py 1")
+
+
+	
